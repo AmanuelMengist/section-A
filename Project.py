bb7927618c28d6c4e36ce3965467b06e307e514a
AmanuelMengist <amanuelmengist90@gmail.com>
2025-03-23 16:34:49 +0300

    first day commit

---
+++ Project.py
@@ -0,0 +1,8 @@
+def stringToLowerCase(string):
+    print(string.lower())
+    
+def stringToUpperCase(string):
+    print(string.upper())
+    
+def stringToTitleCase(string):
+    print(string.title() ) 
